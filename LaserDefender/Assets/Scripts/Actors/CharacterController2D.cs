5fe1854ed1d8626da69cef37b943e5157078dada
Albert Alma <albert.alma@king.com>
2017-12-26 11:14:30 +0100

    Added SpacheShip with CharacterController

---
+++ LaserDefender/Assets/Scripts/Actors/CharacterController2D.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	public void ApplyHorizontalMovement(float speed) {
+		transform.position += new Vector3 (speed * Time.deltaTime, 0, 0);
+	}
+}
