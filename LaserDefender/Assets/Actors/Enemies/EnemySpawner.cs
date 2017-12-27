6a64be038145466046ba41d7f06a88f3f92dd13f
Albert Alma <albert.alma@king.com>
2017-12-27 18:22:19 +0100

    Enemy formation and instantiating first enemy

---
+++ LaserDefender/Assets/Actors/Enemies/EnemySpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject enemy;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate(enemy, new Vector3(0,0,0), Quaternion.identity, transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
